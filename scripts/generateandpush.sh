b994854252be3cffe77bbb22cd399123de761da7
PatrickMenoti <82882574+PatrickMenoti@users.noreply.github.com>
2023-05-30 17:54:30 -0300

    chore: test two repos

---
+++ scripts/generateandpush.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+types=("go" "php")
+
+git config --global user.email "patrick.menoti@gmail.com"
+git config --global user.name "PatrickMenoti"
+for type in ${types[@]}; do
+    for file in ${{ steps.changed-files.outputs.all_changed_files }}; do
+        removed=$(echo ${file} | cut -d. -f1);
+        npx @openapitools/openapi-generator-cli generate -i $file -g ${type} -o ./${type}/${removed} --package-name ${removed};\
+    done
+    cd ${type}
+    git checkout -b generated-sdk
+    git add .
+    git commit -m "Auto-Generated SDK"
+    git push origin generated-sdk
+    gh pr create --body "Auto-generated SDK" --title "Auto-generated SDK"
+    cd ..
+done
