60d6951aad51261160223cc7d618f5fb1e5b6ece
Vander Vieira <vandervsvieira@gmail.com>
2023-12-13 12:03:33 -0300

    add new apis to spectral overrides rules

--- spectral/functions/checkNumberMaximumLimit.js
+++ spectral/functions/checkNumberMaximumLimit.js
@@ -1,7 +1,7 @@
 export default property => {
     const errors = [];
 
-    if (property.type === 'number' || property.type === 'integer' && !property.nullable && !property.readOnly) {
+    if ((property.type === 'number' || property.type === 'integer') && !property.nullable && !property.readOnly && !property.enum) {
         if (property.maximum === undefined) {
             errors.push({
                 message: `Missing maximum limit for numeric property`
--- spectral/functions/checkNumberMinimumLimit.js
+++ spectral/functions/checkNumberMinimumLimit.js
@@ -1,7 +1,7 @@
 export default property => {
     const errors = [];
 
-    if (property.type === 'number' || property.type === 'integer' && !property.nullable && !property.readOnly) {
+    if ((property.type === 'number' || property.type === 'integer') && !property.nullable && !property.readOnly && !property.enum) {
         if (property.minimum === undefined) {
             errors.push({
                 message: `Missing minimum limit for numeric property`
